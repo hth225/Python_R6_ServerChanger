67fdeca642e5f4d5a6ad7c273f8546f722229a9c
hth225 <hth225@naver.com>
2017-06-16 10:31:53 +0900

    in progress

--- changer.py
+++ changer.py
@@ -1,18 +1,30 @@
-def mainmenu(location):
-    fileloc = open(location, 'r')
-    addre = fileloc
+def mainmenu():
+    fileloc = open("location.txt", 'r')
+    for i in fileloc.readline():
+        addre = i
     fileloc.close()
 
-    servloc = open(addre, 'r')
+    servloc = open(addre, 'r+')
+    for i, info in enumerate(servloc):
+        if "DataCenterHint=" in enumerate(servloc):
+            print(info)
 
-    if location is None:
-        save_file_location()
-    else:
-        print("Your server is "+servloc.find("DataCenterHint="))
+    servloc.close()
 
 def save_file_location():
     location = input("Enter GameSettings.ini file locaion :")
-    wr = open("locaion.txt", "w")
+    wr = open("location.txt", "w")
     wr.write(location)
     wr.close()
     return location
+
+if __name__ == '__main__':
+    # save_file_location()
+    file = open("location.txt", 'r')
+    content = file.read()
+    if content == None:
+        save_file_location()
+    else:
+        mainmenu()
+
+    file.close()
