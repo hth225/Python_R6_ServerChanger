c2433af1ee801e9b5e866a5defd39b5d2a331a46
hth225 <hth225@naver.com>
2017-06-17 23:58:55 +0900

    address handling changed

--- changer.py
+++ changer.py
@@ -1,10 +1,10 @@
 def mainmenu():
-    fileloc = open("location.txt", 'r')
-    for i in fileloc.readline():
-        addre = i
-    fileloc.close()
+    # fileloc = open("location.txt", 'r')
+    # # for i in fileloc.readline():
 
-    servloc = open(addre, 'r+')
+    # fileloc.close()
+
+    servloc = open("C:\\Users\\LAD\\Documents\\My Games\\Rainbow Six - Siege\\88b82571-82f9-49bd-a0df-7576dd6f39b2\\GameSettings.ini", 'r+')
     for i, info in enumerate(servloc):
         if "DataCenterHint=" in enumerate(servloc):
             print(info)
